650c4f99d92781029cad15528e6fb6d02cd814e3
Lautaro <dielauts@gmail.com>
2022-10-22 19:09:45 -0300

    Agregando todas las views al ej3

--- app/Views/homeView.php
+++ app/Views/homeView.php
@@ -1,20 +1,81 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
+    <title>Club de Rugby</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Club Rugby</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
+
+    <link rel="stylesheet" href="app\Views\styleGeneral.css">
+    <link rel="stylesheet" href="app\Views\homeStyle.css">
 </head>
 
 <body>
 
-    <h1>HOME</h1>
+    <div class="container-fluid p-1 bg-dark text-white text-center mt-5 titulo">
+        <h1><b>|~</b> Clubcito de Rugby <b>~|</b></h1>
+        <p></p>
+    </div>
 
-    <a href="http://localhost/Club_Rugby/index.php/socioController/create">CREATE</a>
-    <a href="http://localhost/Club_Rugby/index.php/socioController/mostrar">READ</a>
+    <div class="container">
+        <h2 class="text-center mt-2">Seleccione una opcion</h2>
+        <div class="row mt-4">
+            <div class="col-sm-6">
+                <div class="d-grid">
+                    <a type="button" class="btn btn-lg btn-outline-dark" href="http://localhost/Club_Rugby/index.php/socioController/create">Inscribirse</a>
+                </div>
+                <p class="mt-3" style="font-size: 27;">Si es la primera vez que va a participar del torneo de rugby o
+                    actualmente no se
+                    encuentra
+                    inscripto. Por favor, presione este boton para que pueda hacerlo
+                </p>
+            </div>
+            <div class="col-sm-6 text-end">
+                <div class="d-grid">
+                    <a type="button" class="btn btn-lg btn-outline-warning" href="http://localhost/Club_Rugby/index.php/socioController/mostrar">Mostrar socios</a>
 
+                </div>
+                <p class="mt-3">Al presionar este boton, se lograra ver una tabla con los socios del clubcito de Rugby
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="text-center mt-5 mb-2">
+        <h2>Solo una pasion</h2>
+    </div>
+    <div class="container text-center">
+        <div class="row  align-items-center  imagenes">
+            <div class="col-sm-4 p-3">
+                <img src="app\Views\img\balon.jpg" style="width: 90%;" alt="">
+            </div>
+            <div class="col-sm-4 p-3">
+                <img src="app\Views\img\puntaso.jpg" style="width: 90%;" alt="">
+            </div>
+            <div class="col-sm-4 p-3">
+                <img src="app\Views\img\locos.jpg" style="width: 95%;" alt="">
+            </div>
+        </div>
+    </div>
+
+
+
+    <!--Waves Container-->
+    <div class="fixed-bottom">
+        <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+            <defs>
+                <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+            </defs>
+            <g class="parallax">
+                <use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7" />
+                <use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.5)" />
+                <use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)" />
+                <use xlink:href="#gentle-wave" x="48" y="7" fill="#fff" />
+            </g>
+        </svg>
+    </div>
+    <!--Waves end-->
 </body>
 
 </html>
--- app/Views/socioView/mostrarSocioView.php
+++ app/Views/socioView/mostrarSocioView.php
@@ -1,76 +1,98 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
+    <title>Club de Rugby</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Club Rugby</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="http://localhost/Club_Rugby/app/Views/styleGeneral.css">
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 
 <body>
-    <!-- <label class="form-label">Buscar:</label>
-    <div class="mb-3">
-        <select class="form-select" aria-label="Select Forma" name="selectForma" id="selectForma">
-            <option value="nombre">Nombre</option>
-            <option value="numero_sucursal">Numero de Sucursal</option>
-        </select>
-    </div>
-    <div class="mb-3">
-        <input type="text" class="form-control" id="inputValor" name="inputValor">
-    </div>
-    <button type="submit" class="btn btn-primary">Buscar</button> -->
-    <form>
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Nombre</th>
-                    <th scope="col">Documento</th>
-                    <th scope="col">Telefono</th>
-                    <th scope="col">Direccion</th>
-                    <th scope="col">Mail</th>
-                    <th scope="col">Categoria</th>
-                </tr>
-            </thead>
-            <tbody class="table-group-divider">
-                <?php for ($i = 0; $i < sizeof($socios); $i++) { ?>
-                    <tr>
-                        <th scope="row"><?php echo $i + 1 ?></th>
-                        <td><?php echo $socios[$i]['nombre_apellido'] ?></td>
-                        <td><?php echo $socios[$i]['dni'] ?></td>
-                        <td><?php echo $socios[$i]['telefono'] ?></td>
-                        <td><?php echo $socios[$i]['direccion'] ?></td>
-                        <td><?php echo $socios[$i]['mail'] ?></td>
-                        <td><?php
-                            if ($socios[$i]['id_categoria'] == 1) {
-                                echo 'Infantil';
-                            }
-                            if ($socios[$i]['id_categoria'] == 2) {
-                                echo 'Juvenil';
-                            }
-                            if ($socios[$i]['id_categoria'] == 3) {
-                                echo 'Mayor';
-                            }
-                            if ($socios[$i]['id_categoria'] == 4) {
-                                echo 'Veterano';
-                            }
-                            ?></td>
-                    </tr>
-                <?php } ?>
-            </tbody>
-        </table>
-        <button type="submit" class="btn btn-primary">Eliminar</button>
-    </form>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+
+    <div class="container-fluid p-1 bg-dark text-white text-center mt-5 titulo">
+        <h1><b>|~</b> Clubcito de Rugby <b>~|</b></h1>
+        <p></p>
+    </div>
+
+    <div class="container mt-4">
+        <h2 class="text-center mt-5 text-dark">Listado de socios
+        </h2>
+        <div class="row mt-4 formulario">
+            <form>
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Nombre</th>
+                            <th scope="col">Documento</th>
+                            <th scope="col">Telefono</th>
+                            <th scope="col">Direccion</th>
+                            <th scope="col">Mail</th>
+                            <th scope="col">Categoria</th>
+                        </tr>
+                    </thead>
+                    <tbody class="table-group-divider">
+                        <?php for ($i = 0; $i < sizeof($socios); $i++) { ?>
+                            <tr>
+                                <th scope="row"><?php echo $i + 1 ?></th>
+                                <td><?php echo $socios[$i]['nombre_apellido'] ?></td>
+                                <td><?php echo $socios[$i]['dni'] ?></td>
+                                <td><?php echo $socios[$i]['telefono'] ?></td>
+                                <td><?php echo $socios[$i]['direccion'] ?></td>
+                                <td><?php echo $socios[$i]['mail'] ?></td>
+                                <td><?php
+                                    if ($socios[$i]['id_categoria'] == 1) {
+                                        echo 'Infantil';
+                                    }
+                                    if ($socios[$i]['id_categoria'] == 2) {
+                                        echo 'Juvenil';
+                                    }
+                                    if ($socios[$i]['id_categoria'] == 3) {
+                                        echo 'Mayor';
+                                    }
+                                    if ($socios[$i]['id_categoria'] == 4) {
+                                        echo 'Veterano';
+                                    }
+                                    ?></td>
+                            </tr>
+                        <?php } ?>
+                    </tbody>
+                </table>
+                <button type="submit" class="btn btn-primary">Eliminar</button>
+            </form>
+
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+
+
+        </div>
+    </div>
+
+
+    <!--Waves Container-->
+    <div class="fixed-bottom">
+        <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+            <defs>
+                <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+            </defs>
+            <g class="parallax">
+                <use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7" />
+                <use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.5)" />
+                <use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)" />
+                <use xlink:href="#gentle-wave" x="48" y="7" fill="#fff" />
+            </g>
+        </svg>
+    </div>
+    <!--Waves end-->
+
+
+
 </body>
-
-
-
-<!--
-    Cliente: Nombre y Apellido – Dirección – Teléfono – Fecha de Nacimiento –
-    Documento de Identidad – CUIT/CUIL
--->
 
 </html>
