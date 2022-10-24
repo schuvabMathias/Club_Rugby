7f6ce0b7548a70bf266b93164824ec5cbcdcf262
Mathias <mathiasarcem@gmail.com>
2022-10-23 22:26:05 -0300

    Changes two

--- app/Views/socioView/createSocioView.js
+++ app/Views/socioView/createSocioView.js
@@ -1,3 +1,28 @@
+var select = document.querySelector("#selectCategoria");
+
+
+if (select.value == 1) {
+    document.querySelector('.infantil-Nomb').style.display = "block";
+    document.querySelector('.infantil-Doc').style.display = "block";
+    document.querySelector('.veterano').style.display = "none";
+    document.querySelector('.veteranoInv').style.display = "none";
+}
+
+if (select.value == 2 || select.value == 3) {
+    document.querySelector('.infantil-Nomb').style.display = "none";
+    document.querySelector('.infantil-Doc').style.display = "none";
+    document.querySelector('.veterano').style.display = "none";
+    document.querySelector('.veteranoInv').style.display = "none";
+}
+
+if (select.value == 4) {
+    document.querySelector('.infantil-Nomb').style.display = "none";
+    document.querySelector('.infantil-Doc').style.display = "none";
+    document.querySelector('.veterano').style.display = "block";
+    document.querySelector('.veteranoInv').style.display = "block";
+}
+
+
 document.querySelector("#selectCategoria").addEventListener("change", function () {
     document.querySelector('.infantil-Nomb').style.display = this.value == 1 ? "block" : "none";
     document.querySelector('.infantil-Doc').style.display = this.value == 1 ? "block" : "none";
